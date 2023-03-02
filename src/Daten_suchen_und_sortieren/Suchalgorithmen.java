1dc0777ade3138cb6bf3a5abdf78afabb1da07c0
HitoHitoNika <mbalthasar0@gmail.com>
2023-03-02 08:19:21 +0100

    Voll Impl

--- src/Daten_suchen_und_sortieren/Suchalgorithmen.java
+++ src/Daten_suchen_und_sortieren/Suchalgorithmen.java
@@ -1,11 +1,18 @@
 package Daten_suchen_und_sortieren;
 
 public class Suchalgorithmen {
+    //Siehe Moodle PowerPoint "Daten suchen"
     public static void main(String[] args) {
         //Beispiel Sequentielle Suche
         int[] a={5,2,4,3,1};
         System.out.println("Die 7 ist in dem Array enthalten :"+seqSuche(a, 7));
         System.out.println("Die 3 ist in dem Array enthalten :"+seqSuche(a, 3));    
+    
+        //Beispiel Binäre Suche
+        //Achtung das Array muss bereits sortiert sein
+        int[] b={1,2,3,4,5};
+        System.out.println("Die Zahl 7 ist im Array enthalten :"+binSuche(a, 7));
+        System.out.println("Die Zahl 3 ist im Array enthalten :"+binSuche(a, 3));
     }
     
     static boolean seqSuche(int[] a , int gesucht){
@@ -18,7 +25,22 @@ public class Suchalgorithmen {
     }
 
     static boolean binSuche(int[]a, int x){
-        
+        int links = 0;
+        int rechts = a.length-1;    
+        int mitte;
+        while(links<=rechts){
+            mitte=links+((rechts-links)/2);
+            if(a[mitte]==x){
+                return true;
+            }else{
+                if(x<a[mitte]){
+                    rechts=mitte-1;
+                }else{
+                    links=mitte+1;
+                }
+            }
+        }
+        return false;
     }
 
 }
