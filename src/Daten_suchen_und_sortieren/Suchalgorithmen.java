304075c1a4243d8f7dbe037c52bc192c3929fca2
HitoHitoNika <mbalthasar0@gmail.com>
2023-03-02 08:29:48 +0100

    Kommentare geaddet

--- src/Daten_suchen_und_sortieren/Suchalgorithmen.java
+++ src/Daten_suchen_und_sortieren/Suchalgorithmen.java
@@ -7,12 +7,18 @@ public class Suchalgorithmen {
         int[] a={5,2,4,3,1};
         System.out.println("Die 7 ist in dem Array enthalten :"+seqSuche(a, 7));
         System.out.println("Die 3 ist in dem Array enthalten :"+seqSuche(a, 3));    
-    
+        //Im besten Fall liegt die Zahl auf der 0ten Stelle, also muss das Array nicht komplett durchlaufen werden
+        //Im schlechtesten Fall liegt die gesuchte Zahl auf der letzten Stelle, man muss also das komplette Array durchlaufen
+
+
+
         //Beispiel Binäre Suche
         //Achtung das Array muss bereits sortiert sein
         int[] b={1,2,3,4,5};
         System.out.println("Die Zahl 7 ist im Array enthalten :"+binSuche(a, 7));
         System.out.println("Die Zahl 3 ist im Array enthalten :"+binSuche(a, 3));
+        //Im besten Fall liegt die gesuchte in der Mitte des Arrays
+        //Im schlechtesten Fall an Anfang oder Ende
     }
     
     static boolean seqSuche(int[] a , int gesucht){
