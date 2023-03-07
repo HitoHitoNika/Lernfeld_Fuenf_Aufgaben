8cb95d772fbf7f27e60d88022b1ef0fa46ed4186
HitoHitoNika <mbalthasar0@gmail.com>
2023-03-07 19:45:35 +0100

    Methoden Demo aus Moodle Implementiert

---
+++ src/Methoden/Demo_Methoden.java
@@ -0,0 +1,13 @@
+package Methoden;
+
+public class Demo_Methoden {
+
+	public static void main(String[] args) {
+	//Allgemeiner Aufbau einer Methode
+		//public/ private -> Sichtbarkeit
+		//static -> keine Objektinstanzen benötigt
+		//z.Bsp. void -> bedeutet leere Rückgabe
+		//main -> Methodenname
+		//(Übergagebeparameter)
+	}
+}
---
+++ src/Methoden/Demo_Methoden_Scope.java
@@ -0,0 +1,15 @@
+package Methoden;
+
+public class Demo_Methoden_Scope {
+	
+	//1.Methodendefinition
+	public static void helloWorldAusgabe() {
+		System.out.println("Hello World!");
+	}
+	
+	public static void main(String[] args) {
+		//2.Methodenaufruf
+		helloWorldAusgabe();
+	}
+	
+}
