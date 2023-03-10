3dcb62260bca17bff6f8cfe50c40062fd6c9e992
Fawaz Thomas Bouti <32436688+Achellous@users.noreply.github.com>
2023-03-10 19:25:14 +0000

    Sysou für Probleme behandlung

--- src/einfachedatentypen/grundlagen/Datentypen.java
+++ src/einfachedatentypen/grundlagen/Datentypen.java
@@ -17,6 +17,8 @@ public class Datentypen {
         // Intialisierung ("Wert abspeichern")
         temperatur = 32.57;
         zeichen = '@';
+        
+        System.out.println(temperatur + "\n" + zeichen);
 
         // Deklaration und Initialisierung
         byte alter = 19;
@@ -24,9 +26,18 @@ public class Datentypen {
         float groesse = 1.66f;
         double kranHoehe = 73.7;
         boolean javaIstToll = true;
+
+        System.out.println(alter + "\n" +  groesse + "\n" + grosseZahl 
+        + "\n" +kranHoehe + "\n" +javaIstToll);
+
+        aufgabeEins();
+        aufgabeZwei();
+        aufgabeDrei();
+        
+        
     }
 
-    void aufgabeEins() {
+    static void aufgabeEins() {
         // Aufgabe 1
         // Deklarieren Sie eine int-Variable und speichern Sie
         // die Zahl 129 darin ab. Speichern Sie in einer float-Variable
@@ -35,27 +46,41 @@ public class Datentypen {
         int zahl = 129;
         float gleitkommaZahl = 3.14f;
         double coolereGleitkommaZahl = 1618;
+
+        System.out.println(zahl + "\n" + gleitkommaZahl + "\n" + coolereGleitkommaZahl);
     }
 
-    void aufgabeZwei() {
+    static void aufgabeZwei() {
         // Aufgabe 2
         // Ihr Alter, ihre Körpergroesse, die Information ob etwas schwarz oder weiß ist
         // Das $ Zeichen, die Anzahl der Wochentage
         int meinAlter = 21;
         double meineGroesse = 2.01;
         boolean istSchwarz;
+        istSchwarz = true;
         boolean istWeiss;
+        istWeiss = false;
         char character = '$';
         int anzahlDerWochentage = 7;
+
+        System.out.println(meinAlter + "\n" + meineGroesse + "\n" + meinAlter
+        + "\n" + character + "\n" + anzahlDerWochentage);
+        System.out.println (istSchwarz + "\n" + istWeiss );
     }
 
-    void aufgabeDrei() {
+
+    static void aufgabeDrei() {
         // Aufgabe 3
         // Legen Sie für drei selbstgewählte Daten sinnvolle
         // Speicherplätze an und belegen Sie diese mit Werten.
-        double kontostand;
-        float notenDurschnitt;
+        double kontostand = 10.25;
+        float notenDurschnitt = 2.5f;
         char geschlecht = 'm';
+
+        System.out.println(kontostand);
+        System.out.println(notenDurschnitt);
+        System.out.println(geschlecht);
+
 
     }
 }
