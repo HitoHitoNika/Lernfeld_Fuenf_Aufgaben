2d8f7fc23618259e6fa232a7f1c3a1b4b3ba3356
HitoHitoNika <mbalthasar0@gmail.com>
2023-03-10 22:19:01 +0100

    Author

--- src/methoden/DemoMethoden.java
+++ src/methoden/DemoMethoden.java
@@ -1,5 +1,14 @@
 package methoden;
 
+/**
+ * Methoden Demo
+ * 
+ * @autor Max B / github.com/hitohitonika
+ * @version 1.3
+ * @datum 1.03.2023
+ */
+
+
 public class DemoMethoden {
 
 	public static void main(String[] args) {
--- src/methoden/DemoMethodenScope.java
+++ src/methoden/DemoMethodenScope.java
@@ -1,5 +1,15 @@
 package methoden;
 
+/**
+ * Demo Methoden Scope
+ * 
+ * @autor Max B / github.com/hitohitonika
+ * @version 1.3
+ * @datum 1.03.2023
+ */
+
+
+
 public class DemoMethodenScope {
 	
 	//1.Methodendefinition
